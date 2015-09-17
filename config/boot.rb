808126b145517fa51f2a5915c0e5c4c0951084c7
christiano <christiano.rodrigues@vagas.com.br>
2015-09-17 19:10:49 -0300

    starting front end

--- config/boot.rb
+++ config/boot.rb
@@ -3,7 +3,9 @@ require 'json'
 require_relative '../app/exceptions/invalid_pick'
 require_relative '../app/lib/game'
 require_relative '../app/endpoints/game'
+require_relative '../app/endpoints/main'
 
 configure do
+  set :public_folder, File.join(File.dirname(__FILE__), '../public')
   set :show_exceptions, false
 end
---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('myApp', []);
+
+//add a controller to it
+app.controller('MyCtrl', function($scope, $http) {
+  //a scope function to load the data.
+  $scope.loadData = function () {
+    $http.get('/Your/Sinatra/Route').success(function(data) {
+      $scope.items = data;
+    });
+  };
+});
